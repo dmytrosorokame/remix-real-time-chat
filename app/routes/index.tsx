cc21513547836f513c1d06682bbefd43c4516434
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-27 18:49:41 +0200

    add supabase project types

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,16 @@
 import { useLoaderData } from "@remix-run/react";
 import supabase from "utils/supabase";
 
-export const loader = async () => {
+import type { LoaderArgs } from "@remix-run/node";
+
+export const loader = async ({}: LoaderArgs) => {
   const { data } = await supabase.from("messages").select();
 
   return { data };
 };
 
 export default function Index() {
-  const { data } = useLoaderData();
+  const { data } = useLoaderData<typeof loader>();
 
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
