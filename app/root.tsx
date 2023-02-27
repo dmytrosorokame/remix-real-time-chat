23ea7a384c2f81a47ca205f6995d56b9938138ed
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-27 19:51:35 +0200

    add auto-update auth state after login/logout

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useRevalidator,
 } from "@remix-run/react";
 import createServerSupabase from "utils/supabase.server";
 import { createBrowserClient } from "@supabase/auth-helpers-remix";
@@ -44,18 +45,28 @@ export const loader = async ({ request }: LoaderArgs) => {
 
 export default function App() {
   const { env, session } = useLoaderData<typeof loader>();
-
-  console.log({ server: { session } });
+  const revalidator = useRevalidator();
 
   const [supabase] = useState(() =>
     createBrowserClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
   );
 
+  const serverAccessToken = session?.access_token;
+
   useEffect(() => {
-    supabase.auth
-      .getSession()
-      .then((session) => console.log({ client: { session } }));
-  }, []);
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.access_token !== serverAccessToken) {
+        // call loader
+        revalidator.revalidate();
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [supabase, serverAccessToken, revalidator]);
 
   return (
     <html lang="en">
