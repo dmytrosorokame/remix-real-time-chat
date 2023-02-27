5a7a4cc0d87bc9de1457b94eec6cd62c3195f5cb
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-27 20:16:40 +0200

    add insert messages logic

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,32 @@
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import createServerSupabase from "utils/supabase.server";
 import Login from "components/login";
 
-import type { LoaderArgs } from "@remix-run/node";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
+
+export const action = async ({ request }: ActionArgs) => {
+  const response = new Response();
+  const supabase = createServerSupabase({ request, response });
+
+  const { message } = Object.fromEntries(await request.formData());
+  const { error } = await supabase
+    .from("messages")
+    .insert({ content: String(message) });
+
+  if (error) {
+    console.log(error);
+  }
+
+  return json(null, { headers: response.headers });
+};
 
 export const loader = async ({ request }: LoaderArgs) => {
   const response = new Response();
   const supabase = createServerSupabase({ request, response });
   const { data } = await supabase.from("messages").select();
 
-  return json({ messages: data ?? [] }, { header: response.headers });
+  return json({ messages: data ?? [] }, { headers: response.headers });
 };
 
 export default function Index() {
@@ -20,6 +36,10 @@ export default function Index() {
     <>
       <Login />
       <pre>{JSON.stringify(messages, null, 2)}</pre>
+      <Form method="post">
+        <input type="text" name="message" />
+        <button type="submit">Send</button>
+      </Form>
     </>
   );
 }
