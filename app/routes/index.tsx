4344602c2661a0f1e986f048d662837bb9feb24d
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-27 18:50:34 +0200

    change data var name

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,11 +6,11 @@ import type { LoaderArgs } from "@remix-run/node";
 export const loader = async ({}: LoaderArgs) => {
   const { data } = await supabase.from("messages").select();
 
-  return { data };
+  return { messages: data ?? [] };
 };
 
 export default function Index() {
-  const { data } = useLoaderData<typeof loader>();
+  const { messages } = useLoaderData<typeof loader>();
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <pre>{JSON.stringify(messages, null, 2)}</pre>;
 }
